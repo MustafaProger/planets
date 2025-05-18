bae869ea8337ea7dc7509cb55e08691efd898614
Mustafa <diertojiev@yandex.ru>
2025-05-18 16:58:55 +0300

    1.16 Настройка деплоя на Netlify с корректной структурой проекта

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+export default defineConfig({
+	root: "src",
+	build: {
+		outDir: "../dist",
+		rollupOptions: {
+			output: {
+				entryFileNames: "js/[name].js",
+				chunkFileNames: "js/[name].js",
+				assetFileNames: (assetInfo) => {
+					if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+						return "styles/[name][extname]";
+					}
+					return "assets/[name][extname]";
+				},
+			},
+		},
+	},
+	plugins: [
+		viteStaticCopy({
+			targets: [
+				{ src: "img", dest: "img" },
+				{ src: "fonts", dest: "fonts" },
+				{ src: "js", dest: "js" },
+				{ src: "styles", dest: "styles" },
+			],
+		}),
+	],
+});
